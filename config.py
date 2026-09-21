f1dbf9e4b0661f723f63d700cae6e521fda34dcc
sheetuzt <sheetuzt23@gmail.com>
2026-09-21 17:43:45 +0530

    Update config.py with hardcoded API credentials
    Replaced environment variable fetching with hardcoded values for BOT_TOKEN, APP_ID, and API_HASH.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("8912937343:AAE7yuy5SJEEF7Pi5KDPnysKRQffiIFswNw")
+APP_ID = int(os.environ.get("37604662"))
+API_HASH = os.environ.get("8727d3e28416f5c28cd60bf2c892d984")
 
 youtube_next_fetch = 0  # time in minute
 
