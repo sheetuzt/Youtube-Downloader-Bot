1d02757eca8f5aaa67d13ee9558c4dec5362a430
sheetuzt <sheetuzt23@gmail.com>
2026-09-21 18:04:03 +0530

    Change environment variable name for APP_ID

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import  os
 
 BOT_TOKEN = os.environ.get("8912937343:AAE7yuy5SJEEF7Pi5KDPnysKRQffiIFswNw")
-APP_ID = int(os.environ.get("37604662"))
+APP_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("8727d3e28416f5c28cd60bf2c892d984")
 
 youtube_next_fetch = 0  # time in minute
